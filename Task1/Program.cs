e672c85ddacbf1a17cc3e568eec5a9bca8ff265c
Karma <quiteplacedev@gmail.com>
2024-05-27 20:13:05 +0500

    Added validation in constructors, some comments and changed OutputType to Library.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -13,6 +13,11 @@ namespace GeometryLibrary
 
         public Circle(double radius)
         {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Radius must be greater than zero.");
+            }
+
             this.radius = radius;
         }
 
@@ -28,6 +33,17 @@ namespace GeometryLibrary
 
         public Triangle(double side1, double side2, double side3)
         {
+            if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+            {
+                throw new ArgumentException("All sides must be greater than zero.");
+            }
+
+            // Проверка треугольного неравенства
+            if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+            {
+                throw new ArgumentException("Invalid triangle from given sides.");
+            }
+
             this.side1 = side1;
             this.side2 = side2;
             this.side3 = side3;
@@ -43,6 +59,9 @@ namespace GeometryLibrary
         {
             double[] sides = { side1, side2, side3 };
             Array.Sort(sides);
+
+            // С помощью Array.Sort мы знаем, что под индексом 2 находится наибольшая сторона треугольника (гипотенуза)
+            // Массив изменился, потому что он ссылочного типа. Мы не передаем копию, а ссылку на массив в Array.Sort
             return Math.Pow(sides[2], 2) == Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2);
         }
     }
@@ -54,21 +73,4 @@ namespace GeometryLibrary
             return shape.CalculateArea();
         }
     }
-
-    // class Program
-    // {
-    //     static void Main()
-    //     {
-    //         Circle circle = new(3);
-    //         double circleArea = ShapeCalculator.CalculateArea(circle);
-    //         Console.WriteLine($"Circle area: {circleArea}");
-
-    //         Triangle triangle = new(3, 4, 5);
-    //         double triangleArea = ShapeCalculator.CalculateArea(triangle);
-    //         Console.WriteLine($"Triangle area: {triangleArea}");
-
-    //         bool isRightAngled = triangle.IsRightAngled();
-    //         Console.WriteLine($"Is the triangle right-angled? {isRightAngled}");
-    //     }
-    // }
 }
