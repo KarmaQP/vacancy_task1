d0620a1a1de12c46e5c2464d315718fdb47fb0cb
Karma <quiteplacedev@gmail.com>
2024-05-27 19:46:41 +0500

    Updated .gitignore to remove bin/Debug

--- GeometryLibrary.Tests/obj/Debug/net8.0/GeometryLibrary.Tests.AssemblyInfo.cs
+++ GeometryLibrary.Tests/obj/Debug/net8.0/GeometryLibrary.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeometryLibrary.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7c601749812e0b93f59072a39f0c808ee47ac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeometryLibrary.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeometryLibrary.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
